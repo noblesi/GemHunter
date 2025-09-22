d3018562203bc446f807eab6def62e10be058169
민성 <noblesi1570@gmail.com>
2025-09-22 19:46:51 +0900

    적 오브젝트 제작 및 행동 제어

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap tilemap;
+    [SerializeField]
+    private GameObject[] enemyPrefabs;
+    [SerializeField]
+    private int enemyCount = 10;
+
+    private Vector3 offset = new Vector3(0.5f, 0.5f, 0);
+    private List<Vector3> possibleTiles = new List<Vector3>();
+
+    private void Awake()
+    {
+        tilemap.CompressBounds();
+
+        CalculatePossibleTiles();
+
+        for(int i = 0; i< enemyCount; i++)
+        {
+            int type = Random.Range(0, enemyPrefabs.Length);
+            int index = Random.Range(0, possibleTiles.Count);
+
+            Instantiate(enemyPrefabs[type], possibleTiles[index], Quaternion.identity, transform);
+        }
+    }
+
+    private void CalculatePossibleTiles()
+    {
+        BoundsInt bounds = tilemap.cellBounds;
+        TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+
+        for(int y = 1; y < bounds.size.y - 1; ++y)
+        {
+            for(int x = 1; x < bounds.size.x - 1; ++x)
+            {
+                TileBase tile = allTiles[y * bounds.size.x + x];
+
+                if(tile != null)
+                {
+                    Vector3Int localPosition = bounds.position + new Vector3Int(x, y);
+                    Vector3 position = tilemap.CellToWorld(localPosition) + offset;
+                    position.z = 0;
+
+                    possibleTiles.Add(position);
+                }
+            }
+        }
+    }
+}
