75d553b9b6fd5fb01534a1a1840a5e5216b2438e
민성 <noblesi1570@gmail.com>
2025-09-24 23:38:27 +0900

    공격 스킬 [Lightning Strike] 추가

---
+++ Assets/Scripts/Skill/Projectile/ProjectileLightningStrike.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ProjectileLightningStrike : ProjectileGlobal
+{
+    [SerializeField]
+    private GameObject projectile;
+
+    public override void SetUp(SkillBase skillBase, float damage)
+    {
+        base.SetUp(skillBase, damage);
+    }
+
+    public override void Process()
+    {
+        for(int i = 0; i < EnemySpawner.Enemies.Count; ++i)
+        {
+            if (EnemySpawner.Enemies[i] != null) continue;
+
+            Instantiate(projectile, EnemySpawner.Enemies[i].MiddlePoint, Quaternion.identity);
+            TakeDamage(EnemySpawner.Enemies[i]);
+        }
+
+        Destroy(gameObject);
+    }
+}
